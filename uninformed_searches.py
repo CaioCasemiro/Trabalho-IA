6528be33f4633a325496098535b70f6f1e95d677
CaioCasemiro <casemirocaio2@gmail.com>
2025-06-14 13:50:02 -0300

    implementação da dls e ids, faltando alguns ajustes

--- uninformed_searches.py
+++ uninformed_searches.py
@@ -35,3 +35,39 @@ def bfs (estado_inicial, objetivo, tamanho_lado): #busca em largura
                 fronteira.append(filho)
 
     print("Nenhuma solução encontrada.")
+
+def dls (estado_inicial, objetivo, tamanho_lado, limite): #busca em profundidade
+
+    estado_inicial_obj = PuzzleState(estado_inicial)
+    fronteira = [estado_inicial_obj]
+    visitados = set()
+    visitados.add(tuple(estado_inicial))
+
+    while fronteira:
+        estado_atual = fronteira.pop()
+        if estado_atual.is_objetivo(objetivo):
+            return estado_atual
+        if estado_atual.profundidade < limite:
+            filhos = estado_atual.gerar_filhos(tamanho_lado)
+            for filho in filhos:
+                estado_tuple = tuple(filho.estado)
+                if estado_tuple not in visitados:
+                    visitados.add(estado_tuple)
+                    fronteira.append(filho)
+    return None
+
+def ids(estado_inicial, objetivo, tamanho_lado, limite_max=50): #aprofundamento iterativo
+    print("\nExecutando Aprofundamento Iterativo (IDS)...")
+    inicio = time.time()
+    for limite in range(limite_max + 1):
+        resultado = dls(estado_inicial, objetivo, tamanho_lado, limite)
+        if resultado:
+            fim = time.time()
+            caminho = resultado.caminho()
+            print("\nObjetivo alcançado!")
+            print("Sequência de movimentos:", caminho)
+            print("Quantidade de passos:", len(caminho))
+            print(f"Tempo de execução: {fim - inicio:.4f} segundos")
+            print("Profundidade da solução:", resultado.profundidade)
+            return
+    print("Nenhuma solução encontrada.")
